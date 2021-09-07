50dd326e6130382f0b8d55f81b94130c2aaa292f
Manuel Ocampo <75048521+ManuelOg16@users.noreply.github.com>
2021-09-07 18:23:29 -0500

    primer upload del sitio con bootstrap

---
+++ index.js
@@ -0,0 +1,4 @@
+//Una forma de inicializar todas las descripciones emergentes de una página sería seleccionarlas por su data-toggleatributo:
+$(function (){
+    $('[data-toggle="tooltip"]').tooltip()
+})
